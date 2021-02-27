fc2d0c48812f526a4de431f5edb6d99f7b821c9b
mohammad rafi <mohd.rafi0195@gmail.com>
2021-02-27 12:53:37 +0530

    First Added by Mohammad Rafi

---
+++ force-app/main/default/lwc/lwcInputCmp/lwcInputCmp.js
@@ -0,0 +1,15 @@
+import { LightningElement ,track} from 'lwc';
+
+export default class LwcInputCmp extends LightningElement {
+    @track name; //  declared a track decorator
+    
+    nameChange(e){ 
+        console.log('====::value'+e.target.value); 
+        console.log('====::name'+e.target.name); 
+        // change handler method call when any change happen in the ligtning input feild to read input in lwc using  property
+        if(e.target.name ==='input'){
+            console.log('====::'+e.target.value);   
+            this.name = e.target.value;
+        }
+    }
+}
